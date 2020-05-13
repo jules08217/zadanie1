87f911885cc5c75799edfbf1ee6e20c45f49056f
jules08217 <59442979+jules08217@users.noreply.github.com>
2020-05-13 16:04:27 +0500

    Add files via upload

---
+++ ConsoleApp5/LIb1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class LIb1
+{
+    static public string stpow(string a, int b)
+    {
+        string s = "";
+        for(int i = 0; i < b; i++)
+        {
+            s = s + a;
+        }
+        return s;
+    }
+    static public string rev(string a)
+    {
+        char[] rev = a.ToCharArray();
+        Array.Reverse(rev);
+        a = new string(rev);
+        return a;
+    }
+    static public string remov(string str, string s)
+    {
+        str = str.Replace(s, "");
+        return str;
+    }
+    static public int leng(string a)
+    {
+        int l = a.Length;
+        return l;
+    }
+}
+
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static LIb1;
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1: " + LIb1.stpow("la",9));
+            Console.WriteLine("2: " + LIb1.rev("fly"));
+            Console.WriteLine("3: " + LIb1.remov("butterfly", "t"));
+            Console.WriteLine("4: " + LIb1.leng("butterfly"));
+        }
+    }
+}
